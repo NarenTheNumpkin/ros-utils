0e05ba82645f662e607303319f565a88a37bead2
Naren <137981148+Wxua2005@users.noreply.github.com>
2025-06-20 16:06:11 +0000

    Added rpk for faster creation of ros-packages

--- src/cwp.sh
+++ src/cwp.sh
@@ -17,3 +17,19 @@ cwp() {
     echo "Package not found."
     return 1
 }
+
+rpk() {
+    PKG_NAME="$1"
+    PKG_TYPE="$2"
+
+    if [[ -z "$PKG_NAME" ]]; then
+        echo "Specify package name please !"
+        return 1
+    fi
+
+    if [[ -z "$PKG_TYPE" ]]; then
+        ros2 pkg create --build-type ament_python "$PKG_NAME" --dependencies std_msgs geometry_msgs rclpy rclcpp
+    else
+        ros2 pkg create --build-type ament_"${PKG_TYPE}" "$PKG_NAME" --dependencies std_msgs geometry_msgs rclpy rclcpp
+    fi
+}
